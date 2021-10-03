ee9621eedb51b01780dcfeaf67dfacc98e6fd32b
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2021-10-03 16:16:16 +0100

    Expanded readme

--- wk4_linked_list.py
+++ wk4_linked_list.py
@@ -15,6 +15,28 @@ commented out, just to demonstrate the possibility.
 For clarity, I have used the words 'head' and 'tail' to refer only to the ends 
 of the entire list. The head/tail of individual nodes are described as the
 node's 'item' and 'next'.
+
+The four functions we were asked to add (here named add(), remove(), size() and
+search()) all rely on traversal of the list. For this reason, add, size and
+search have an worst-case complexity of O(n). The remove function has to 
+traverse the list up to 3 times (to select nodes either side of the target, so
+they can be linked) and so has a worst case scenario approaching O(3n). Removing
+the head node works in constant time, as only the head of the list is 
+referenced. Removing the tail node only recquires traversing the list twice, so
+has a complexity of O(2n). 
+
+Traversal in these functions is realised either by placing a series of 
+if -statements inside a while-true loop, or else by calling to another function 
+(ie self.final_node, self.at_location) that implements its own traversal based 
+on the same principle. Size is implemented as a recursive function that passes 
+the current count of nodes onto another version of itself via argument. 
+
+As traversal is so central to the methods, I considered implementing an __iter__
+method for the LinkedList class. However, I decided that the extra processing 
+power needed for a method call wasn't adequately made up for by the ability to
+use "list.iter()" instead of "node = node.next". 
+
+
 '''
 
 
@@ -108,21 +130,22 @@ class LinkedList:
             del(node)
 
     def size(self, node="not specified", count=0,):
-        while True:
-            if node == "not specified":
-                node = self.head
-            if node == None:
-                return count
-            elif node.is_tail():
-                count += 1
-                return count
-            else:
-                count += 1
-                node = node.next
-                count = self.size(node, count)
-                return count
+        """returns size of list"""
+        if node == "not specified":
+            node = self.head
+        if node == None:
+            return count
+        elif node.is_tail():
+            count += 1
+            return count
+        else:
+            count += 1
+            node = node.next
+            count = self.size(node, count)
+            return count
 
     def search(self, item, node="not specified", place=0):
+        "returns location of node containing searched for item"
         while True:
             if node == "not specified":
                 node = self.head
