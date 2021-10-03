e35a08133b58de3ccd6af208475df67667add0ca
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2021-10-03 15:17:50 +0100

    rename variables for readability

--- wk4_linked_list.py
+++ wk4_linked_list.py
@@ -9,8 +9,8 @@ of the list type. This didn't feel like the spirit of the assignment.
 The example code given this week included getters and setter methods in its
 classes. As none of the variables in my classes are hidden, I felt a more
 pythonic approach to the problem was to directly reference variables (self.next
-as opposed to self.get_next()). If I was writing in another language, or had a 
-need for stronger encapsulation, I would reimplement the get/set functions.
+as opposed to self.get_next()). I have left the get/set functions below, 
+commented out, just to demonstrate the possibility.
 
 For clarity, I have used the words 'head' and 'tail' to refer only to the ends 
 of the entire list. The head/tail of individual nodes are described as the
@@ -23,15 +23,6 @@ class Node:
         self.item = item
         self.next = None
 
-    def get_next(self):
-        return self.next
-
-    def set_item(self, item):
-        self.item = item
-
-    def set_next(self, next):
-        self.next = next
-
     def is_tail(self):
         if self.next == None:
             return True
@@ -40,6 +31,15 @@ class Node:
 
     def __str__(self):
         return self.item
+
+    # def get_next(self):
+    #     return self.next
+
+    # def set_item(self, item):
+    #     self.item = item
+
+    # def set_next(self, next):
+    #     self.next = next
 
 
 class LinkedList:
@@ -54,7 +54,7 @@ class LinkedList:
             if node.is_tail():
                 return node
             else:
-                node = node.get_next()
+                node = node.next
 
     def is_empty(self):
         if self.head == None:
@@ -62,11 +62,11 @@ class LinkedList:
         else:
             return False
 
-    def location(self, int, node="default", count=0):
+    def at_location(self, int, node="not specified", count=0):
         if self.is_empty():
             print("list is empty")
             return False
-        if node == "default":
+        if node == "not specified":
             node = self.head
         while True:
             if count == int:
@@ -84,32 +84,32 @@ class LinkedList:
             self.head = new_node
         else:
             tail = self.final_node()
-            tail.set_next(new_node)
+            tail.next = new_node
 
     def remove(self, position):
         if position == 0:   # removes head node
             node = self.head
-            post_node = self.location(1)
+            post_node = self.at_location(1)
             self.head = post_node
             del(node)
 
         elif position == self.size() - 1:  # removes tail node
-            node = self.location(position)
-            pre_node = self.location(position-1)
+            node = self.at_location(position)
+            pre_node = self.at_location(position-1)
             pre_node.next = None
             del(node)
 
         else:  # removes other nodes
-            node = self.location(position)
-            pre_node = self.location(position-1)
-            post_node = self.location(position+1)
+            node = self.at_location(position)
+            pre_node = self.at_location(position-1)
+            post_node = self.at_location(position+1)
 
             pre_node.next = post_node
             del(node)
 
-    def size(self, node="default", count=0,):
+    def size(self, node="not specified", count=0,):
         while True:
-            if node == "default":
+            if node == "not specified":
                 node = self.head
             if node == None:
                 return count
@@ -118,13 +118,13 @@ class LinkedList:
                 return count
             else:
                 count += 1
-                node = node.get_next()
+                node = node.next
                 count = self.size(node, count)
                 return count
 
-    def search(self, item, node="default", place=0):
+    def search(self, item, node="not specified", place=0):
         while True:
-            if node == "default":
+            if node == "not specified":
                 node = self.head
             elif node == None:
                 print("item not found")
@@ -178,7 +178,7 @@ if __name__ == "__main__":
 
     # find node at location (used to simplify deletion)
     shape(list, 5)
-    node = list.location(2)
+    node = list.at_location(2)
     print("item at position 2 is " + node.item)
 
     # delete  inner node
