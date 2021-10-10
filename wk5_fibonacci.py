f9dde5b8d45e301fa0b5dacc218793a31c080681
BenjaminGordonCody <b.gordoncody@gmail.com>
2021-10-10 17:34:15 +0100

    better naming of functions

--- wk5_fibonacci.py
+++ wk5_fibonacci.py
@@ -9,10 +9,10 @@ My second attempt(concise_fib) took the opposite approach and started by
 defining the nth term by calling itself for the two previous positions. This
 code felt more elegant, but I suspected it would take more resources.
 
-My first function has O(n), because it calls itself once for every position
-between 0 and n.
+My first function has O(i), because it calls itself once for every position
+between 0 and i.
 
-My second function has O(2^n), because each instance of the function has to call
+My second function has O(2^i), because each instance of the function has to call
 two further instances to get its return value. This renders the function
 impractical very quickly.
 
@@ -22,50 +22,49 @@ implementation took to find the 40th value of the sequence.
 fibonacci performed 40 function calls in under 0.001 seconds
 concise_fib performed 1,099,511,627,776 function calls in 27.83 seconds
 
-My initial function is the more efficient one. 
+My initial function is the more efficient one."""
 
-"""
 # imports
 import time  # used for testing time performance of functions
 
 
-def fibonacci(int, fig_a=0, fig_b=0, count=0):
+def fibonacci(i, fig_a=0, fig_b=0, count=0):
     """
     Returns the value of a position in the Fibonacci sequence.
     Figure A and Figure B are the values of the two previous positions.
-    'int' is the position we are trying to discover.
+    'i' is the position we are trying to discover.
     """
     # checking the input can be understood
-    if int < 1:
+    if i < 1:
         return "usage: only accepts positive integers"
-    if int == 1:
+    if i == 1:
         return 1
 
     # increment counter
     count += 1
 
     # Value of current position
-    sum = fig_a + fig_b
+    current_value = fig_a + fig_b
 
     # If we have calculated the desired number, print it
-    if count == int:
-        return sum
+    if count == i:
+        return current_value
 
     # special case for first value of sequence
-    elif count == 1:
-        return fibonacci(int, 0, 1, count)
+    if count == 1:
+        return fibonacci(i, 0, 1, count)
     else:
-        return fibonacci(int, fig_b, sum, count)
+        return fibonacci(i, fig_b, current_value, count)
 
 
-def concise_fib(n):
-    """returns nth potion of Fibonacci sequence"""
-    if n == 0:
+def concise_fib(i):
+    """returns position i of Fibonacci sequence"""
+    if i == 0:
         return 0
-    if n == 1:
+    if i == 1:
         return 1
     else:
-        return concise_fib(n-1) + concise_fib(n-2)
+        return concise_fib(i-1) + concise_fib(i-2)
 
 
 if __name__ == "__main__":
@@ -84,7 +83,8 @@ if __name__ == "__main__":
         print("'concise_fib' accurately identifies 20th position as 6765")
 
     # time tests for each function
-    n = 40  # position in sequence to request from functions
-    print(f"'fibonacci' took {timetaken(fibonacci, n)}s to find position {n}")
+    i_to_test = 40  # position in sequence to request from functions
     print(
-        f"'concise_fib' took {timetaken(concise_fib, n)}s to find position {n}")
+        f"'fibonacci' took {timetaken(fibonacci, i_to_test)}s to find position {i_to_test}")
+    print(
+        f"'concise_fib' took {timetaken(concise_fib, i_to_test)}s to find position {i_to_test}")
